70cf8cdabecb01ee07b80e3fc11352d76a8dca12
Robert Wen <robert.wen@nyu.edu>
2015-08-03 11:24:50 -0400

    add two test templates for python2 and python3 to test the api client

---
+++ tests/test_client.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+unittest code for Clarifai API Python Client
+"""
+
+import unittest
+from clarifai.client import ClarifaiApi
+
+class TestClarifaiApi(unittest.TestCase):
+
+  def test_api_connection(self):
+    pass
+
+  def test_tag_one_image(self):
+    pass
+
+  def test_tag_bulk_image(self):
+    pass
+
+  def test_tag_one_gif(self):
+    pass
+
+  def test_tag_one_video(self):
+    pass
+
+
+if __name__ == '__main__':
+  unittest.main()
+
