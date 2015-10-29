c6203a3e1d5b7387b087543d95121946b700ab01
Matthew Zeiler <zeiler@clarifai.com>
2015-10-29 10:48:16 -0400

    Revert "refer to the server for languages"

--- clarifai/client/__init__.py
+++ clarifai/client/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import
 
-from .client import ClarifaiApi, ApiError, ApiThrottledError, ApiBadRequestError, ApiClientError
+from .client import ClarifaiApi, ApiError, ApiThrottledError, ApiBadRequestError
