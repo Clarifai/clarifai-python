d9cafea59958df02c03991c9eed2daf1e6b1b309
Robert Wen <robert@clarifai.com>
2017-02-20 22:05:57 +0800

    update test scripts

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,25 +2,24 @@
 # -*- coding: utf-8 -*-
 
 """
-unittest for Clarifai API Python Client
+unittest for Clarifai API v1 Python Client
 """
 
 import os
 import glob
+import mock
 import hashlib
 import unittest
+import clarifai
 from clarifai.client import ClarifaiApi, ApiError
 
 class TestClarifaiApi(unittest.TestCase):
   """
   test the Clarifai API Python client with all supported features
   """
-
-  image_url = 'http://clarifai-img.s3.amazonaws.com/test/toddler-flowers.jpeg'
-  video_url = 'http://techslides.com/demos/sample-videos/small.mp4'
-
-  def get_client(self, *args, **kwargs):
-    return ClarifaiApi()
+  image_url = 'https://samples.clarifai.com/toddler-flowers.jpeg'
+  # video source: http://techslides.com/demos/sample-videos/small.mp4
+  video_url = 'https://samples.clarifai.com/small.mp4'
 
   def test_api_connection(self):
     api = ClarifaiApi()
@@ -35,14 +34,14 @@ class TestClarifaiApi(unittest.TestCase):
   def test_tag_one_image(self):
     """ tag one image, from url and disk """
     # tag image from online URL
-    image_url = 'http://clarifai-img.s3.amazonaws.com/test/toddler-flowers.jpeg'
+    image_url = 'https://samples.clarifai.com/toddler-flowers.jpeg'
     api = ClarifaiApi()
     response = api.tag_image_urls(image_url)
     self.assertTrue(response)
     self.assertTrue(response['results'][0]['url'] == image_url)
 
     # tag image from local fs
-    image_file = 'tests/data/toddler-flowers.jpeg'
+    image_file = 'tests/v1_tests/data/toddler-flowers.jpeg'
     api = ClarifaiApi()
     if os.path.exists(image_file):
       with open(image_file, 'rb') as fb:
@@ -52,8 +51,8 @@ class TestClarifaiApi(unittest.TestCase):
   def test_tag_images(self):
     """ tag multiple images, from url and disk """
     # tag images from online URL
-    image_url_base = 'http://clarifai-img.s3.amazonaws.com/test'
-    image_files = ['metro-north.jpg', 'octopus.jpg', 'tahoe.jpg', 'thai-market.jpg']
+    image_url_base = 'https://samples.clarifai.com'
+    image_files = ['metro-north.jpg', 'tahoe.jpg', 'thai-market.jpg']
     image_urls = [os.path.join(image_url_base, one_file) for one_file in image_files]
 
     api = ClarifaiApi()
@@ -61,8 +60,8 @@ class TestClarifaiApi(unittest.TestCase):
     self.assertTrue(response)
 
     # tag images frmo local fs
-    image_dir = 'tests/data'
-    image_files = ['metro-north.jpg', 'octopus.jpg', 'tahoe.jpg', 'thai-market.jpg']
+    image_dir = 'tests/v1_tests/data'
+    image_files = ['metro-north.jpg', 'tahoe.jpg', 'thai-market.jpg']
 
     api = ClarifaiApi()
     if os.path.exists(image_dir):
@@ -73,8 +72,14 @@ class TestClarifaiApi(unittest.TestCase):
         fd.close()
 
   def test_unicode_urls(self):
-    image_url = u'http://www.alvaronoboa.com/wp-content/uploads/2013/02/Álvaro-Noboa-y-Annabella-Azín-Votaciones-41-1024x682.jpg'
+    image_url = u'https://samples.clarifai.com/Álvaro-Noboa-y-Annabella-Azín-Votaciones.jpg'
+    api = ClarifaiApi()
+    response = api.tag_image_urls(image_url)
+    self.assertTrue(response)
+    self.assertTrue(response['results'][0]['url'] == image_url)
 
+    # test with Chinese in URL
+    image_url = u'https://samples.clarifai.com/长城.jpg'
     api = ClarifaiApi()
     response = api.tag_image_urls(image_url)
     self.assertTrue(response)
@@ -83,14 +88,14 @@ class TestClarifaiApi(unittest.TestCase):
   def test_tag_gif(self):
     """ tag one GIF animation file """
     # source: http://media.giphy.com/media/fRZn2vraBGiA0/giphy.gif
-    image_url = 'http://media.giphy.com/media/fRZn2vraBGiA0/giphy.gif'
+    image_url = 'https://samples.clarifai.com/giphy.gif'
 
     api = ClarifaiApi()
     response = api.tag_image_urls(image_url)
     self.assertTrue(response)
     self.assertTrue(response['results'][0]['url'] == image_url)
 
-    image_file = 'tests/data/water-ocean-turtle.gif'
+    image_file = 'tests/v1_tests/data/water-ocean-turtle.gif'
     api = ClarifaiApi()
     if os.path.exists(image_file):
       with open(image_file, 'rb') as fb:
@@ -98,17 +103,15 @@ class TestClarifaiApi(unittest.TestCase):
         self.assertTrue(response)
 
   def test_tag_one_video(self):
-    # video source: http://techslides.com/demos/sample-videos/small.mp4
-    video_url = 'http://techslides.com/demos/sample-videos/small.mp4'
 
     api = ClarifaiApi()
-    response = api.tag_image_urls(video_url)
+    response = api.tag_image_urls(self.video_url)
     self.assertTrue(response)
-    self.assertTrue(response['results'][0]['url'] == video_url)
+    self.assertTrue(response['results'][0]['url'] == self.video_url)
 
   def test_tag_one_video_from_localfs(self):
     # video source: http://techslides.com/demos/sample-videos/small.mp4
-    video_file = 'tests/data/small.mp4'
+    video_file = 'tests/v1_tests/data/small.mp4'
     api = ClarifaiApi()
     if os.path.exists(video_file):
       with open(video_file, 'rb') as fb:
@@ -122,49 +125,49 @@ class TestClarifaiApi(unittest.TestCase):
 
   # You need special permission to run these tests.
   def test_embed_one_image(self):
-    image_url = 'http://clarifai-img.s3.amazonaws.com/test/toddler-flowers.jpeg'
-    api = self.get_client()
+    image_url = 'https://samples.clarifai.com/toddler-flowers.jpeg'
+    api = ClarifaiApi()
     try:
       response = api.embed_image_urls(image_url)
       self.assertTrue(response)
       self.assertTrue(response['results'][0]['url'] == image_url)
-    except ApiError, e: # User does not have permission.
+    except ApiError as e: # User does not have permission.
       self.check_unauth(e)
 
   def test_embed_one_image_from_localfs(self):
-    image_file = 'tests/data/toddler-flowers.jpeg'
-    api = self.get_client()
+    image_file = 'tests/v1_tests/data/toddler-flowers.jpeg'
+    api = ClarifaiApi()
     if os.path.exists(image_file):
       with open(image_file, 'rb') as fb:
         try:
           response = api.embed_images(fb)
           self.assertTrue(response)
-        except ApiError, e:
+        except ApiError as e:
           self.check_unauth(e)
 
   def test_tag_n_embed_one_image(self):
-    image_url_base = 'http://clarifai-img.s3.amazonaws.com/test'
-    image_files = ['metro-north.jpg', 'octopus.jpg', 'tahoe.jpg', 'thai-market.jpg']
+    image_url_base = 'https://samples.clarifai.com'
+    image_files = ['metro-north.jpg', 'tahoe.jpg', 'thai-market.jpg']
     image_urls = [os.path.join(image_url_base, one_file) for one_file in image_files]
 
-    api = self.get_client()
+    api = ClarifaiApi()
     try:
       response = api.tag_and_embed_image_urls(image_urls)
       self.assertTrue(response)
-    except ApiError, e:
+    except ApiError as e:
       self.check_unauth(e)
 
   def test_tag_n_embed_from_localfs(self):
-    image_dir = 'tests/data'
-    image_files = ['metro-north.jpg', 'octopus.jpg', 'tahoe.jpg', 'thai-market.jpg']
+    image_dir = 'tests/v1_tests/data'
+    image_files = ['metro-north.jpg', 'tahoe.jpg', 'thai-market.jpg']
 
-    api = self.get_client()
+    api = ClarifaiApi()
     if os.path.exists(image_dir):
       image_files = [open(os.path.join(image_dir, one_file), 'rb') for one_file in image_files]
       try:
         response = api.tag_and_embed_images(image_files)
         self.assertTrue(response)
-      except ApiError, e:
+      except ApiError as e:
         self.check_unauth(e)
       finally:
         for fd in image_files:
@@ -173,9 +176,8 @@ class TestClarifaiApi(unittest.TestCase):
   def test_send_feedback(self):
     """ test sending various feedback """
 
-    urls = ['http://clarifai-img.s3.amazonaws.com/test/metro-north.jpg', \
-            'http://clarifai-img.s3.amazonaws.com/test/metro-north.jpg', \
-            'http://clarifai-img.s3.amazonaws.com/test/octopus.jpg']
+    urls = ['https://samples.clarifai.com/metro-north.jpg', \
+            'https://samples.clarifai.com/metro-north.jpg']
 
     api = ClarifaiApi()
 
@@ -202,7 +204,7 @@ class TestClarifaiApi(unittest.TestCase):
   def test_i18n(self):
     api = ClarifaiApi(language='aaa')
 
-    api = self.get_client()
+    api = ClarifaiApi()
     request_body = api._setup_multi_data([], ['urls'], language='en')
     self.assertEqual(request_body['language'], 'en', 'language field was not set')
 
@@ -260,7 +262,7 @@ class TestClarifaiApi(unittest.TestCase):
 
   def test_concept_ids(self):
     """new models should return concept_ids"""
-    api = self.get_client()
+    api = ClarifaiApi()
 
     api.set_model('general-v1.3')
     response = api.tag_image_urls(self.image_url)
@@ -272,16 +274,41 @@ class TestClarifaiApi(unittest.TestCase):
     self.assertTrue(response['results'][0]['result']['tag']['concept_ids'][0][0].startswith('ai_'),
                     "video concept_ids didn't start wit ai_")
 
+  def test_pil_resizing(self):
+    """ test with image resizing """
 
-    api.set_model('general-v1.1')
-    response = api.tag_image_urls(self.image_url)
-    tag = response['results'][0]['result']['tag']
-    self.assertTrue('concept_ids' in tag, 'concept_ids not included in v1.1 model')
+    image_file = 'tests/v1_tests/data/toddler-flowers.jpeg'
+    api = ClarifaiApi(resize=True)
+    orig = api._resize_image_tuple
 
-    response = api.tag_image_urls(self.video_url)
-    tag = response['results'][0]['result']['tag']
-    self.assertTrue('concept_ids' in tag, 'concept_ids not included in v1.1 model')
+    def mock_resize(*args):
+      return orig(args[1])
 
+    with mock.patch('clarifai.client.ClarifaiApi._resize_image_tuple', \
+                    side_effect=mock_resize, autospec=True) as mock_some_method:
+      self.assertTrue(api.resize)
+
+      if os.path.exists(image_file):
+        with open(image_file, 'rb') as fb:
+          response = api.tag_images(fb)
+          self.assertTrue(response)
+
+      self.assertTrue(mock_some_method.called)
+
+  @mock.patch('clarifai.client.ClarifaiApi')
+  def test_no_resizing(self, mock_api):
+    """ test with no image resizing """
+    image_file = 'tests/v1_tests/data/toddler-flowers.jpeg'
+    api = ClarifaiApi(resize=False)
+
+    self.assertFalse(api.resize)
+
+    if os.path.exists(image_file):
+      with open(image_file, 'rb') as fb:
+        response = api.tag_images(fb)
+        self.assertTrue(response)
+
+    self.assertFalse(mock_api._resize_image_tuple.called)
 
 if __name__ == '__main__':
   unittest.main()
