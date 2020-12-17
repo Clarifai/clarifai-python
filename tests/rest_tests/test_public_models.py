e122d843fd88cfda86774ca80b1ed6291dda0729
HarmitMinhas96 <40869862+HarmitMinhas96@users.noreply.github.com>
2020-12-16 19:01:37 -0500

    Remove segmentation model
    The model is internal only and old.

--- tests/rest_tests/test_public_models.py
+++ tests/rest_tests/test_public_models.py
@@ -79,16 +79,3 @@ class TestPublicModels(unittest.TestCase):
   def test_predict_with_wedding_model(self):
     res = self.app.public_models.wedding_model.predict_by_url(url=sample_inputs.METRO_IMAGE_URL)
     self.assertEqual(10000, res['status']['code'])
-
-  def test_predict_with_segmentation_model(self):
-    # only runs this test in prod as the segmentation models only exist in prod
-    if self.app.api.base_url == 'api.clarifai.com':
-      # test car segmentation
-      m = self.app.models.get(model_id='b17ab715bba448228ef916caaaf8d35b')
-      m.model_version = 'fc92eca9d7cb45cc8832ab40ddc95c3c'
-      res = m.predict_by_url(sample_inputs.METRO_IMAGE_URL)
-
-      # ensure that only 'car' concepts are returned
-      for region in res['outputs'][0]['data']['regions']:
-        for concept in region['data']['concepts']:
-          self.assertEqual('car', concept['name'])
