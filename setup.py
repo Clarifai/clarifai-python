8576f1688533474c58301370c51aaaf561485340
Matthew Zeiler <zeiler@clarifai.com>
2022-07-21 15:39:15 -0400

    Generalized the url splitting so it can be re-used for more than just modules (#10)
    * bump to 0.1.11
    
    * splitting all urls
    
    * fix errors
    
    * added tess and fixed order
    
    * refactored things
    
    * bump
    
    * Update clarifai_utils/urls/helper.py
    
    Co-authored-by: Andrey <andrey.solomatin@clarifai.com>
    
    * Update helper.py
    
    * Update requirements.txt
    
    * made the url stripping better
    
    * much better
    
    Co-authored-by: Andrey <andrey.solomatin@clarifai.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ packages = setuptools.find_packages(include=["clarifai_utils*"])
 
 setuptools.setup(
     name="clarifai-utils",
-    version="0.1.10",
+    version="0.1.12",
     author="Clarifai",
     author_email="support@clarifai.com",
     description="Clarifai Python Utilities",
