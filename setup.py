f57f68b1ce7f3ad12a3de5d43f2157989502f42e
Matthew Zeiler <zeiler@clarifai.com>
2022-09-01 23:24:33 -0400

    bump to 0.1.14

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ packages = setuptools.find_packages(include=["clarifai_utils*"])
 
 setuptools.setup(
     name="clarifai-utils",
-    version="0.1.13",
+    version="0.1.14",
     author="Clarifai",
     author_email="support@clarifai.com",
     description="Clarifai Python Utilities",
