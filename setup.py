b28f2f932f29a1f8aced983b3cc594d89f4f46f2
YvetteZ2017 <30578017+YvetteZ2017@users.noreply.github.com>
2023-03-03 02:48:40 -0500

    Version 9.2.0 (#33)
    Co-authored-by: Yvette Zhang <yvette@clarifai.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ packages = setuptools.find_packages(include=["clarifai*"])
 
 setuptools.setup(
     name="clarifai",
-    version="9.1.2",
+    version="9.2.0",
     author="Clarifai",
     author_email="support@clarifai.com",
     description="Clarifai Python Utilities",
@@ -24,6 +24,6 @@ setuptools.setup(
     license="Apache 2.0",
     python_requires='>=3.8',
     install_requires=[
-        "clarifai-grpc>=9.1.2",
+        "clarifai-grpc>=9.2.0",
     ],
     include_package_data=True)
