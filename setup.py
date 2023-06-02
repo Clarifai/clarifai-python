1ce3ff170db12fa97c2340a252992cd1d969a1f9
Matthew Zeiler <zeiler@clarifai.com>
2023-06-02 16:38:21 -0400

    9.5.3 commit (#55)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ packages = setuptools.find_namespace_packages(include=["clarifai*"])
 
 setuptools.setup(
     name="clarifai",
-    version="9.5.2",
+    version="9.5.3",
     author="Clarifai",
     author_email="support@clarifai.com",
     description="Clarifai Python Utilities",
