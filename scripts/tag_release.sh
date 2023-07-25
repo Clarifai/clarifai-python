f6f8e031e94c4372bab93da03f681b9eb234748f
Sai Nivedh <sai.nivedh@clarifai.com>
2023-07-25 13:38:47 +0530

    removed all

--- scripts/tag_release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-version=$(grep 'version' setup.py | grep -o '[0-9].*[0-9].*[0-9]')
-
-release_tag="v${version}"
-
-git tag $release_tag
-git push origin $release_tag
-
--- scripts/update_pypi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -fr build dist
-
-python setup.py bdist_wheel sdist
-
-twine upload dist/*
-
