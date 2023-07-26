6c5cbb08ad83f8dab71d9f1e9dc549ccd23c6f84
Sai Nivedh <sai.nivedh@clarifai.com>
2023-07-26 18:10:02 +0530

    authentication setup

---
+++ clarifai/client/app_api.py
@@ -0,0 +1,20 @@
+from clarifai_grpc.grpc.api import resources_pb2, service_pb2  # noqa: F401
+
+from clarifai.client.base_auth import BaseAuth
+
+
+class AppApi(BaseAuth):
+    """
+    AppApi is a class that provides access to Clarifai API endpoints related to App information.
+    Inherits from BaseAuth for authentication purposes.
+    """
+    def __init__(self, app_id: str, **kwargs):
+        """
+        Initializes a AppApi object.
+        Args:
+            app_id (str): The App ID for the App to interact with.
+            **kwargs: Additional keyword arguments to be passed to the ClarifaiAuthHelper.
+        """
+        self.kwargs = {**kwargs, 'id': app_id}
+        self.app = resources_pb2.App(**self.kwargs)
+        super().__init__(app_id=self.id)
---
+++ clarifai/client/auth/__init__.py
@@ -0,0 +1,4 @@
+from clarifai.client.auth.abc import V2Stub
+from clarifai.client.auth.stub import create_stub
+
+__all__ = ('V2Stub', 'create_stub')
