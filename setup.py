36bf71088fcdef24123e7ecf4e83f424df1c87e9
sanjaychelliah <sanjay.chelliah@clarifai.com>
2023-08-24 23:09:48 +0530

    version-9.7.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ packages = setuptools.find_namespace_packages(include=["clarifai*"])
 
 setuptools.setup(
     name="clarifai",
-    version="9.7.2",
+    version="9.7.3",
     author="Clarifai",
     author_email="support@clarifai.com",
     description="Clarifai Python SDK",
