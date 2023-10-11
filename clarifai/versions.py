9ad1c27cc012086b71367685264637530d454e5f
Sai Nivedh <sainivedh123@gmail.com>
2023-10-11 19:48:11 +0530

    Version 9.9.2

--- clarifai/versions.py
+++ clarifai/versions.py
@@ -1,11 +1,6 @@
 import os
 
-file_path = os.path.abspath(
-    os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "VERSION"))
-with open(file_path, "r") as f:
-  version = f.read().strip()
-
-CLIENT_VERSION = f"{version}"
+CLIENT_VERSION = "9.9.2"
 OS_VER = os.sys.platform
 PYTHON_VERSION = '.'.join(
     map(str, [os.sys.version_info.major, os.sys.version_info.minor, os.sys.version_info.micro]))
