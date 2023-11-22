b4f181278766521e166e5a9c01d7ef45f7c118f5
Sai Nivedh <sainivedh123@gmail.com>
2023-11-22 17:39:47 +0530

    add dataset upload status feature

--- clarifai/datasets/upload/utils.py
+++ clarifai/datasets/upload/utils.py
@@ -1,9 +1,13 @@
 import importlib
 import inspect
 import os
-from typing import Union
+from typing import Dict, Tuple, Type, Union
+
+from clarifai_grpc.grpc.api.service_pb2 import MultiDatasetVersionMetricsGroupResponse
+from google.protobuf.json_format import MessageToDict
 
 from clarifai.datasets.upload.base import ClarifaiDataLoader
+from clarifai.errors import UserError
 
 
 def load_module_dataloader(module_dir: Union[str, os.PathLike], **kwargs) -> ClarifaiDataLoader:
@@ -41,3 +45,200 @@ def load_module_dataloader(module_dir: Union[str, os.PathLike], **kwargs) -> Cla
       continue
 
   return main_module_cls(**kwargs)
+
+
+class DisplayUploadStatus:
+  """Class to display dataset upload status."""
+
+  def __init__(self, dataloader: ClarifaiDataLoader,
+               dataset_metrics_response: Type[MultiDatasetVersionMetricsGroupResponse],
+               dataset_info_dict: Dict[str, str]) -> None:
+    """Initialize the class.
+    Args:
+      dataloader: ClarifaiDataLoader object
+      dataset_metrics_response: The dataset version metrics response from the server.
+      dataset_info_dict: The dataset info dictionary.
+    """
+    self.dataloader = dataloader
+    self.dataset_metrics_response = dataset_metrics_response
+    self.dataset_info_dict = dataset_info_dict
+
+    self.display()
+
+  def display(self) -> None:
+    """Display the upload status."""
+    from rich.console import Console
+
+    local_inputs_count, local_annotations_dict = self.get_dataloader_stats()
+    uploaded_inputs_dict, uploaded_annotations_dict = self.get_uploaded_dataset_stats()
+
+    self.local_annotations_count = sum(local_annotations_dict.values())
+    self.uploaded_annotations_count = sum(uploaded_annotations_dict.values())
+
+    local_dataset_dict = {
+        "Inputs Count": str(local_inputs_count),
+        "Annotations Count": str(local_annotations_dict)
+    }
+    uploaded_dataset_dict = {
+        "Inputs Count": str(uploaded_inputs_dict["total"]),
+        "Annotations Count": str(uploaded_annotations_dict)
+    }
+
+    panel_layout = self.get_display_layout(local_dataset_dict, uploaded_dataset_dict)
+
+    console = Console()
+    console.print(panel_layout)
+
+  def get_dataloader_stats(self) -> Tuple[int, dict[str, int]]:
+    """Get the number of inputs and annotations in a dataloader.
+
+    Returns:
+      local_inputs_count (int): total number of inputs in the dataloader
+      local_annotations_dict (dict[str, int]): total number of annotations in the dataloader
+    """
+    from clarifai.constants.dataset import DATASET_UPLOAD_TASKS
+
+    if not isinstance(self.dataloader, ClarifaiDataLoader):
+      raise UserError("Dataloader is not an instance of ClarifaiDataLoader")
+
+    task = self.dataloader.task
+    if task not in DATASET_UPLOAD_TASKS:
+      raise UserError(
+          "Invalid task, please use one of the following: {}".format(DATASET_UPLOAD_TASKS))
+    local_inputs_count = len(self.dataloader)
+    local_annotations_dict = dict(concepts=0, bboxes=0, polygons=0)
+    for i in range(local_inputs_count):
+      if task == "visual_segmentation":
+        local_annotations_dict["polygons"] += len(self.dataloader[i].polygons)
+      elif task == "visual_detection":
+        local_annotations_dict["bboxes"] += len(self.dataloader[i].bboxes)
+      elif task == "text_classification":
+        local_annotations_dict["concepts"] += len(self.dataloader[i].labels)
+      elif task == "visual_classification" or task == "visual_captioning":
+        local_annotations_dict["concepts"] += len(self.dataloader[i].labels)
+
+    return local_inputs_count, local_annotations_dict
+
+  def get_uploaded_dataset_stats(self) -> Tuple[dict[str, int], dict[str, int]]:
+    """Parse the response from the server for the dataset version metrics groups.
+
+    Returns:
+      uploaded_inputs_dict (dict[str, int]): The input statistics for the dataset.
+      uploaded_annotations_dict (dict[str, int]): The annotation statistics for the dataset.
+    """
+    dataset_statistics = []
+    uploaded_inputs_dict = {}
+    uploaded_annotations_dict = {"concepts": 0, "bboxes": 0, "polygons": 0}
+    dict_response = MessageToDict(self.dataset_metrics_response)
+
+    for data in dict_response["datasetVersionMetricsGroups"]:
+      if isinstance(data["value"], str):
+        if data["value"].startswith("id-"):
+          data["metrics"].update({"Concept": data["value"]})
+          data["metrics"].pop("regionLocationMatrix", None)
+          dataset_statistics.append(data["metrics"])
+        else:
+          uploaded_inputs_dict[data["value"]] = data["metrics"]["inputsCount"]
+
+    for ds in dataset_statistics:
+      uploaded_annotations_dict["bboxes"] += int(ds["boundingBoxesCount"])
+      uploaded_annotations_dict["concepts"] += int(ds["positiveInputTagsCount"])
+      uploaded_annotations_dict["polygons"] += int(ds["polygonsCount"])
+
+    return uploaded_inputs_dict, uploaded_annotations_dict
+
+  def _create_layout(self):
+    from rich.layout import Layout
+    from rich.progress import BarColumn, Progress, TextColumn
+
+    # Create a Layout
+    layout = Layout()
+
+    # Add a new task to the progress bar
+    progress = Progress(
+        "{task.description}",
+        BarColumn(bar_width=100),
+        TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
+    )
+
+    # Split the layout into top and bottom rows
+    layout.split(Layout(name="Progress"), Layout(name="Tables"))
+
+    # Add the progress bar to the top layout
+    layout["Progress"].update(progress)
+
+    # Split the bottom layout into two columns
+    layout["Tables"].split_row(
+        Layout(name="Local Dataset"),
+        Layout(name="Uploaded Dataset"),
+    )
+
+    # Create a new layout for the panels
+    panel_layout = Layout(size=18)
+
+    # Split the panel layout into top and bottom rows
+    panel_layout.split(Layout(name="Progress Panel", size=9), Layout(name="Tables Panel", size=9))
+
+    return layout, panel_layout, progress
+
+  def get_display_layout(self, local_dataset_dict: Dict[str, str],
+                         uploaded_dataset_dict: Dict[str, str]):
+    """Create a layout for the display.
+
+    Args:
+      local_dataset_dict (dict): The local dataset stats info dict.
+      uploaded_dataset_dict (dict): The uploaded dataset stats info dict.
+
+    Returns:
+      panel_layout (Layout): The panel layout for the display.
+    """
+    from rich.console import Group
+    from rich.panel import Panel
+
+    from clarifai.utils.logging import table_from_dict
+
+    local_dataset_table = table_from_dict(
+        [local_dataset_dict],
+        column_names=["Inputs Count", "Annotations Count"],
+        title="[cyan]Local Dataset")
+    uploaded_dataset_table = table_from_dict(
+        [uploaded_dataset_dict],
+        column_names=["Inputs Count", "Annotations Count"],
+        title="[cyan]Uploaded Dataset")
+    dataset_info_table = table_from_dict(
+        [self.dataset_info_dict], column_names=["dataset_id", "user_id", "app_id"])
+
+    layout, panel_layout, progress = self._create_layout()
+
+    # Add a new task to the progress bar
+    progress.add_task(
+        "[cyan]Inputs Progress:",
+        completed=int(uploaded_dataset_dict["Inputs Count"]),
+        total=int(local_dataset_dict["Inputs Count"]))
+    progress.add_task(
+        "[cyan]Annotations Progress:",
+        completed=self.uploaded_annotations_count,
+        total=self.local_annotations_count)
+
+    # Add the tables to the respective layouts
+    layout["Local Dataset"].update(local_dataset_table)
+    layout["Uploaded Dataset"].update(uploaded_dataset_table)
+
+    # Create a render group for the progress bar and the additional data
+    progress_group = Group(progress, dataset_info_table)
+
+    # Create a panel for the progress bar with a blue border and a suitable heading
+    progress_panel = Panel(progress_group, title="[b] Dataset Upload Summary", border_style="blue")
+
+    # Create a panel for the tables comparison with a blue border and a suitable heading
+    tables_panel = Panel(
+        layout["Tables"],
+        title="[b] Dataset Metrics Comparison",
+        border_style="blue",
+        expand=False)
+
+    # Add the panels to the respective layouts
+    panel_layout["Progress Panel"].update(progress_panel)
+    panel_layout["Tables Panel"].update(tables_panel)
+
+    return panel_layout
