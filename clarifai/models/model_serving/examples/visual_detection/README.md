8c05ca2fcb36e81248b931b7a5a884ac012819c9
phtvo <vohungphat99@gmail.com>
2023-12-18 17:36:25 +0700

    init

--- clarifai/models/model_serving/examples/visual_detection/README.md
+++ clarifai/models/model_serving/examples/visual_detection/README.md
@@ -13,3 +13,27 @@ These can be used on the fly with minimal or no changes to test deploy visual de
 		wget -O model.pt https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5x.pt
 		```
 	* zip and test deploy to your Clarifai app
+
+* ### Torch serve model format [yolov8](./yolov8_torchserve/)
+
+	To utilize a Torch serve model (`.mar` file) created by running `torch-model-archiver` – essentially a zip file containing the model checkpoint, Python code, and other components – within this module, follow these steps:
+
+	1. Unzip the `.mar` file to obtain **your checkpoint**.
+	2. Implement your postprocess method in `inference.py`.
+
+	For example: [yolov8](./yolov8_torchserve/), suppose you already have `.mar` file following the [torch serve example](https://github.com/pytorch/serve/blob/master/examples/object_detector/yolo/yolov8/README.md)
+
+	* unzip it to `1/model_store`
+	```bash
+	unzip yolov8n.mar -d 1/model_store/
+	```
+	```bash
+	# in 1/model_store you will have
+	1/model_store/
+	├── MAR-INF
+	│   └── MANIFEST.json
+	├── custom_handler.py
+	└── yolov8n.pt
+	```
+
+	* test, zip and upload the model
---
+++ clarifai/models/model_serving/examples/visual_detection/yolov8_torchserve/1/inference.py
@@ -0,0 +1,83 @@
+# This file contains boilerplate code to allow users write their model
+# inference code that will then interact with the Triton Inference Server
+# Python backend to serve end user requests.
+# The module name, module path, class name & get_predictions() method names MUST be maintained as is
+# but other methods may be added within the class as deemed fit provided
+# they are invoked within the main get_predictions() inference method
+# if they play a role in any step of model inference
+"""User model inference script."""
+
+import os
+from pathlib import Path
+
+import numpy as np
+import torch
+from ultralytics import YOLO
+
+from clarifai.models.model_serving.model_config import ModelTypes, get_model_config
+from clarifai.models.model_serving.models.output import VisualDetectorOutput
+
+config = get_model_config(ModelTypes.visual_detector)
+
+
+class InferenceModel:
+  """User model inference class."""
+
+  def __init__(self) -> None:
+    """
+    Load inference time artifacts that are called frequently .e.g. models, tokenizers, etc.
+    in this method so they are loaded only once for faster inference.
+    """
+    self.base_path: Path = os.path.dirname(__file__)
+    self.checkpoint_path = os.path.join(self.base_path, "/model_store/yolov8n.pt")  #yolov5x
+
+    self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    self.model = YOLO(self.checkpoint_path)
+    self.model.to(self.device)
+
+  @config.inference.wrap_func
+  def get_predictions(self, input_data: list, **kwargs) -> list:
+    """
+    Main model inference method.
+
+    Args:
+    -----
+      input_data: A list of input data item to predict on.
+        Input data can be an image or text, etc depending on the model type.
+
+      **kwargs: your inference parameters.
+
+    Returns:
+    --------
+      List of one of the `clarifai.models.model_serving.models.output types` or `config.inference.return_type(your_output)`. Refer to the README/docs
+    """
+    max_bbox_count = 300  # max allowed detected bounding boxes per image
+    outputs = []
+
+    if isinstance(input_data, np.ndarray) and len(input_data.shape) == 4:
+      input_data = list(input_data)
+
+    predictions = self.model(input_data)
+
+    for inp_data, preds in zip(input_data, predictions):
+      boxes = preds.boxes.xyxy.cpu().numpy()
+      labels = [[pred] for pred in preds.boxes.cls.cpu().numpy()]
+      scores = [[pred] for pred in preds.boxes.conf.cpu().numpy()]
+      h, w, _ = inp_data.shape  # input image shape
+      bboxes = [[x[1] / h, x[0] / w, x[3] / h, x[2] / w]
+                for x in boxes]  # normalize the bboxes to [0,1]
+      if len(bboxes) != 0:
+        bboxes = np.concatenate((bboxes, np.zeros((max_bbox_count - len(bboxes), 4))))
+        scores = np.concatenate((scores, np.zeros((max_bbox_count - len(scores), 1))))
+        labels = np.concatenate((labels, np.zeros(
+            (max_bbox_count - len(labels), 1), dtype=np.int32)))
+      else:
+        bboxes = np.zeros((max_bbox_count, 4), dtype=np.float32)
+        scores = np.zeros((max_bbox_count, 1), dtype=np.float32)
+        labels = np.zeros((max_bbox_count, 1), dtype=np.int32)
+
+      outputs.append(
+          VisualDetectorOutput(
+              predicted_bboxes=bboxes, predicted_labels=labels, predicted_scores=scores))
+
+    return outputs
