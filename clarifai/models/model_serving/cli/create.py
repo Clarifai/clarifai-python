eb4adb1748196ae878a3c47eaee23c42c49c1818
phtvo <vohungphat99@gmail.com>
2024-01-16 16:09:12 +0700

    add info after model created

--- clarifai/models/model_serving/cli/create.py
+++ clarifai/models/model_serving/cli/create.py
@@ -132,3 +132,55 @@ class SubCreateModelCli(BaseClarifaiCli):
 
       triton_repo = TritonModelRepository(model_config)
       triton_repo.build_repository(self.working_dir)
+
+    from itertools import islice
+    from pathlib import Path
+
+    def tree(dir_path: Path,
+             level: int = -1,
+             limit_to_directories: bool = False,
+             length_limit: int = 1000):
+      # prefix components:
+      space = '    '
+      branch = '│   '
+      # pointers:
+      tee = '├── '
+      last = '└── '
+      """Given a directory Path object print a visual tree structure"""
+      dir_path = Path(dir_path)  # accept string coerceable to Path
+      files = 0
+      directories = 0
+
+      def inner(dir_path: Path, prefix: str = '', level=-1):
+        nonlocal files, directories
+        if not level:
+          return  # 0, stop iterating
+        if limit_to_directories:
+          contents = [d for d in dir_path.iterdir() if d.is_dir()]
+        else:
+          contents = list(dir_path.iterdir())
+        pointers = [tee] * (len(contents) - 1) + [last]
+        for pointer, path in zip(pointers, contents):
+          if path.is_dir():
+            yield prefix + pointer + path.name
+            directories += 1
+            extension = branch if pointer == tee else space
+            yield from inner(path, prefix=prefix + extension, level=level - 1)
+          elif not limit_to_directories:
+            yield prefix + pointer + path.name
+            files += 1
+
+      print(dir_path.name)
+      iterator = inner(dir_path, level=level)
+      for line in islice(iterator, length_limit):
+        print(line)
+      if next(iterator, None):
+        print(f'... length_limit, {length_limit}, reached, counted:')
+      print(f'\n{directories} directories' + (f', {files} files' if files else ''))
+
+    print("-" * 75)
+    print(f"* Created repository at: {self.working_dir}")
+    tree(self.working_dir)
+    print()
+    print("* Please make sure your code is tested using `test.py` before uploading")
+    print("-" * 75)
