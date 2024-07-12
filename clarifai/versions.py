5ea2fe17c5d0d19294d4516118d7f48bfebdabd5
Mansi Khamkar <khamkarmansi@gmail.com>
2024-07-12 22:23:08 +0530

    VERSION 10.5.4 (#360)
    * VERSION 10.5.4
    
    * updated changelog
    
    * Update CHANGELOG.md
    
    * lint fix
    
    ---------
    
    Co-authored-by: Sai Nivedh <sainivedh123@gmail.com>

--- clarifai/versions.py
+++ clarifai/versions.py
@@ -1,6 +1,6 @@
 import os
 
-CLIENT_VERSION = "10.5.3"
+CLIENT_VERSION = "10.5.4"
 OS_VER = os.sys.platform
 PYTHON_VERSION = '.'.join(
     map(str, [os.sys.version_info.major, os.sys.version_info.minor, os.sys.version_info.micro]))
