575d5f65057c24b9bd9f93dbe4a060dd522b6fa9
Matthew Zeiler <zeiler@clarifai.com>
2024-09-24 18:14:55 -0400

    check req_id slows through too

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -4,7 +4,7 @@ import logging
 
 from rich.logging import RichHandler
 
-from clarifai.utils.logging import JsonFormatter, _get_library_name, get_logger
+from clarifai.utils.logging import JsonFormatter, _get_library_name, get_logger, set_logger_context
 
 
 def test_get_logger():
@@ -46,9 +46,22 @@ def test_json_logger():
   # assert the ts was within 10 seconds of now (in UTC time)
   assert abs(datetime.datetime.utcnow() - datetime.datetime.strptime(
       ts, "%Y-%m-%dT%H:%M:%S.%fZ")) < datetime.timedelta(seconds=10)
-
   assert result['filename'] == filename
   assert result['msg'] == msg
   assert result['lineno'] == lineno
   assert result['stack_info'] == sinfo
   assert result['level'] == "info"
+  assert 'req_id' not in result
+
+  req_id = "test_req_id"
+  set_logger_context(req_id=req_id)
+  json_line = jf.format(r)
+  result = json.loads(json_line)
+  assert abs(datetime.datetime.utcnow() - datetime.datetime.strptime(
+      ts, "%Y-%m-%dT%H:%M:%S.%fZ")) < datetime.timedelta(seconds=10)
+  assert result['filename'] == filename
+  assert result['msg'] == msg
+  assert result['lineno'] == lineno
+  assert result['stack_info'] == sinfo
+  assert result['level'] == "info"
+  assert result['req_id'] == req_id
