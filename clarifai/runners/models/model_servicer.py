ae180f7d27713243a0ade0440ca9a8a8330a056f
David Eigen <deigen@clarifai.com>
2025-01-21 22:58:17 +0000

    download in parallel to current stream

--- clarifai/runners/models/model_servicer.py
+++ clarifai/runners/models/model_servicer.py
@@ -1,10 +1,9 @@
-from itertools import tee
 from typing import Iterator
 
 from clarifai_grpc.grpc.api import service_pb2, service_pb2_grpc
 from clarifai_grpc.grpc.api.status import status_code_pb2, status_pb2
 
-from ..utils.url_fetcher import ensure_urls_downloaded
+from ..utils.url_fetcher import ensure_urls_downloaded, map_stream
 
 
 class ModelServicer(service_pb2_grpc.V2Servicer):
@@ -66,15 +65,13 @@ class ModelServicer(service_pb2_grpc.V2Servicer):
     This is the method that will be called when the servicer is run. It takes in an input and
     returns an output.
     """
-    # Duplicate the iterator
-    request, request_copy = tee(request)
 
     # Download any urls that are not already bytes.
-    for req in request:
-      ensure_urls_downloaded(req)
+    def _download_urls_stream(requests):
+      yield from map_stream(ensure_urls_downloaded, requests)
 
     try:
-      return self.model.stream_wrapper(request_copy)
+      return self.model_class.stream(_download_urls_stream(request))
     except Exception as e:
       yield service_pb2.MultiOutputResponse(status=status_pb2.Status(
           code=status_code_pb2.MODEL_PREDICTION_FAILED,
