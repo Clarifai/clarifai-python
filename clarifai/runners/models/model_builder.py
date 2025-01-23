a29661e71c0c2a4aa3ecae069441d5b3b1e75378
David Eigen <deigen@clarifai.com>
2025-01-23 16:49:25 +0000

    include main for now to reduce diff

--- clarifai/runners/models/model_builder.py
+++ clarifai/runners/models/model_builder.py
@@ -607,3 +607,20 @@ class ModelBuilder:
             f"\nModel build failed with status: {resp.model_version.status} and response {resp}")
         return False
 
+
+def main(folder, download_checkpoints, skip_dockerfile):
+  buidler = ModelBuilder(folder)
+  if download_checkpoints:
+    buidler.download_checkpoints()
+  if not skip_dockerfile:
+    buidler.create_dockerfile()
+  exists = buidler.check_model_exists()
+  if exists:
+    logger.info(
+        f"Model already exists at {buidler.model_url}, this upload will create a new version for it."
+    )
+  else:
+    logger.info(f"New model will be created at {be.model_url} with it's first version.")
+
+  input("Press Enter to continue...")
+  buidler.upload_model_version(download_checkpoints)
