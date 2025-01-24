33dd0f2d33cd4986aa679cd21bd62545da037d02
luv-bansal <bansal22luvi@gmail.com>
2025-01-24 07:48:56 +0000

    Added hf model run locally tests

--- tests/runners/test_model_run_locally.py
+++ tests/runners/test_model_run_locally.py
@@ -205,3 +205,68 @@ def test_docker_build_and_test_container(model_run_locally):
 
     # Remove the image
     model_run_locally.remove_docker_image(image_name)
+
+
+def test_hf_test_model_success(hf_model_run_locally):
+  """
+  Test that test_model succeeds with the dummy model.
+  This calls the script's test_model method, which runs a subprocess.
+  """
+  hf_model_run_locally.uploader.download_checkpoints()
+  hf_model_run_locally.create_temp_venv()
+  hf_model_run_locally.install_requirements()
+
+  # Catch the subprocess call. If the dummy model is correct, exit code should be 0.
+  try:
+    hf_model_run_locally.test_model()
+  except SystemExit:
+    pytest.fail("test_model() triggered a system exit with non-zero code.")
+  except subprocess.CalledProcessError:
+    # If the process didn't return code 0, fail the test
+    pytest.fail("The model test did not complete successfully in the subprocess.")
+  finally:
+    # Clean up
+    hf_model_run_locally.clean_up()
+
+
+# @pytest.mark.skipif(shutil.which("docker") is None, reason="Docker not installed or not in PATH.")
+@pytest.mark.skip(reason="Will add later after new clarifai package is released")
+def test_hf_docker_build_and_test_container(hf_model_run_locally):
+  """
+  Test building a Docker image and running a container test using the dummy model.
+  This test will be skipped if Docker is not installed.
+  """
+
+  # Download the checkpoints for the model
+  hf_model_run_locally.uploader.download_checkpoints()
+
+  # Test if Docker is installed
+  assert hf_model_run_locally.is_docker_installed(), "Docker not installed, skipping."
+
+  # Build or re-build the Docker image
+  hf_model_run_locally.uploader.create_dockerfile()
+  image_tag = hf_model_run_locally._docker_hash()
+  image_name = f"{hf_model_run_locally.config['model']['id']}:{image_tag}"
+
+  if not hf_model_run_locally.docker_image_exists(image_name):
+    hf_model_run_locally.build_docker_image(image_name=image_name)
+
+  # Run tests inside the container
+  try:
+    hf_model_run_locally.test_model_container(
+        image_name=image_name,
+        container_name="test_clarifai_model_container",
+        env_vars={
+            'CLARIFAI_PAT': CLARIFAI_PAT,
+            'CLARIFAI_API_BASE': os.environ.get('CLARIFAI_API_BASE', 'https://api.clarifai.com')
+        })
+  except subprocess.CalledProcessError:
+    pytest.fail("Failed to test the model inside the docker container.")
+  finally:
+    # Clean up the container if it still exists
+    if hf_model_run_locally.container_exists("test_clarifai_model_container"):
+      hf_model_run_locally.stop_docker_container("test_clarifai_model_container")
+      hf_model_run_locally.remove_docker_container("test_clarifai_model_container")
+
+    # Remove the image
+    hf_model_run_locally.remove_docker_image(image_name)
