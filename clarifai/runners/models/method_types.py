bd7ced3510c894e45628ca696dc45a465b99e1a3
David Eigen <deigen@clarifai.com>
2025-02-10 16:18:07 -0500

    edits

--- clarifai/runners/models/method_types.py
+++ clarifai/runners/models/method_types.py
@@ -1,5 +1,5 @@
 import io
-from typing import List, Tuple, get_args, get_origin
+from typing import List, get_args, get_origin
 
 import numpy as np
 from clarifai_grpc.grpc.api import resources_pb2
@@ -11,12 +11,8 @@ def build_function_serializers(func):
   '''
     Build serializers for the given function.
     '''
-  types = dict(func.__annotations__)
-  output_type = types.pop('return', None)
-  if len(types) == 1:
-    input_type = list(types.values())[0]
-  else:
-    input_type = Input(**types)
+  input_type = dict(func.__annotations__)
+  output_type = input_type.pop('return', None)
   input_serializer = build_serializer(input_type)
   output_serializer = build_serializer(output_type)
   return input_serializer, output_serializer
@@ -36,7 +32,7 @@ def build_serializer(type_annotation):
   if get_origin(type_annotation) == list:
     inner_type = get_args(type_annotation)[0]
     if inner_type in _SERIALIZERS:
-      return ListSerializer(_SERIALIZERS[inner_type])
+      return PartsList(_SERIALIZERS[inner_type])
     raise NotImplementedError(f'List of {inner_type} not supported')
 
   # named parts fields
@@ -45,7 +41,7 @@ def build_serializer(type_annotation):
     #names, types = zip(*type_annotation.fields.items())
     names = type_annotation._fields
     types = [getattr(type_annotation, name) for name in names]
-    return PartsSerializer(names, types, cls)
+    return PartsDict(names, types, cls)
 
 
 class Parts:
@@ -61,15 +57,7 @@ class Parts:
     super().__setattr__(name, value)
 
 
-class Input(Parts):
-  pass
-
-
-class Output(Parts):
-  pass
-
-
-class Serializer:
+class Message:
 
   def serialize(self, data, proto=None):
     raise NotImplementedError
@@ -78,7 +66,7 @@ class Serializer:
     raise NotImplementedError
 
 
-class PartsSerializer(Serializer):
+class PartsDict(Message):
 
   def __init__(self, names, types, python_type=dict):
     self.python_type = python_type
@@ -91,8 +79,7 @@ class PartsSerializer(Serializer):
       proto = resources_pb2.Data()
     for name, serializer in self.fields.items():
       part = proto.parts.add()
-      #part.id = name  # TODO add id to parts
-      part.data.metadata['name'] = name
+      part.name = name
       part_data = getattr(data, name)
       serializer.serialize(part_data, part.data)
     return proto
@@ -100,14 +87,12 @@ class PartsSerializer(Serializer):
   def deserialize(self, proto):
     data = {}
     for part in proto.parts:
-      #part_name = part.id  # TODO add id field to parts
-      part_name = part.data.metadata['name']
-      serializer = self.fields[part_name]
-      data[part_name] = serializer.deserialize(part.data)
+      serializer = self.fields[part.name]
+      data[part.name] = serializer.deserialize(part.data)
     return self.python_type(**data)
 
 
-class ListSerializer(Serializer):
+class PartsList(Message):
 
   def __init__(self, serializer, python_type=list):
     self.serializer = serializer
@@ -118,7 +103,7 @@ class ListSerializer(Serializer):
       proto = resources_pb2.Data()
     for i, item in enumerate(data):
       part = proto.parts.add()
-      #part.id = str(i)  # TODO add id to parts
+      #part.name = str(i)  # TODO should we put this in for lists?
       self.serializer.serialize(item, part.data)
     return proto
 
@@ -129,20 +114,19 @@ class ListSerializer(Serializer):
     return ret
 
 
-class DataField(Serializer):
+class DataMessageField(Message):
 
-  def __init__(self, field_name, serializer, is_list=False, is_tuple=False):
+  def __init__(self, field_name, serializer, is_list=False):
     assert field_name in resources_pb2.Data.DESCRIPTOR.fields_by_name, f'Field {field_name} not found in Data proto'
     self.field_name = field_name
     self.serializer = serializer
     self.is_list = is_list
-    self.is_tuple = is_tuple
     descriptor = resources_pb2.Data.DESCRIPTOR.fields_by_name[field_name]
     self.field_is_message = (descriptor.type == descriptor.TYPE_MESSAGE)
     self.field_is_repeated = (descriptor.label == descriptor.LABEL_REPEATED)
-    if self.is_list or self.is_tuple:
-      assert self.field_is_repeated, f'Field {field_name} must be repeated to be a list or tuple'
-    if self.field_is_repeated and (self.is_list or self.is_tuple):
+    if self.is_list:
+      assert self.field_is_repeated, f'Field {field_name} must be repeated to be a list'
+    if self.field_is_repeated and self.is_list:
       self._serialize_field = self._serialize_repeated_list
     elif self.field_is_repeated:
       self._serialize_field = self._serialize_repeated_singleton
@@ -183,8 +167,6 @@ class DataField(Serializer):
     src = getattr(proto, self.field_name)
     if self.field_is_repeated:
       data = [self.serializer.deserialize(item) for item in src]
-      if self.is_tuple:
-        return tuple(data)
       if self.is_list:
         return data
       return data[0]  # singleton case
@@ -192,7 +174,7 @@ class DataField(Serializer):
       return self.serializer.deserialize(src)
 
 
-class TextSerializer(Serializer):
+class TextMessage(Message):
 
   def __init__(self, python_type=str):
     self.python_type = python_type
@@ -211,7 +193,7 @@ class TextSerializer(Serializer):
     raise ValueError(f'Unsupported text type: {self.python_type}')
 
 
-class BytesSerializer(Serializer):
+class BytesMessage(Message):
 
   def serialize(self, data, proto=None):
     assert proto is None
@@ -221,7 +203,7 @@ class BytesSerializer(Serializer):
     return proto  # atomic bytes
 
 
-class NDArraySerializer(Serializer):
+class NDArrayMessage(Message):
 
   def __init__(self, python_type=np.ndarray):
     if get_origin(python_type) == list:
@@ -246,7 +228,7 @@ class NDArraySerializer(Serializer):
     return array
 
 
-class ImageSerializer(Serializer):
+class ImageMessage(Message):
 
   def __init__(self, python_type=None):
     self.python_type = python_type
@@ -281,7 +263,7 @@ class ImageSerializer(Serializer):
     raise ValueError(f'Unsupported image type: {self.python_type}')
 
 
-class ConceptSerializer(Serializer):
+class ConceptMessage(Message):
 
   def serialize(self, data, proto=None):
     if proto is None:
@@ -293,51 +275,54 @@ class ConceptSerializer(Serializer):
 
 
 _SERIALIZERS = {
-    # basic atomic types
+    # str and bytes types
     str:
-        DataField('text', TextSerializer(str)),
-    #bytes:
-    #    DataField('ndarray', BytesSerializer(bytes)),
+        DataMessageField('text', TextMessage(str)),
+    bytes:
+        DataMessageField('bytes', BytesMessage(bytes)),
+    List[str]:
+        PartsList(DataMessageField('text', TextMessage(str))),
+
+    # numeric types
     float:
-        DataField('ndarray', NDArraySerializer(float)),  # TODO too much overhead?
+        DataMessageField('ndarray', NDArrayMessage(float)),  # TODO too much overhead?
     int:
-        DataField('ndarray', NDArraySerializer(int)),
+        DataMessageField('ndarray', NDArrayMessage(int)),
     np.ndarray:
-        DataField('ndarray', NDArraySerializer(np.ndarray)),
+        DataMessageField('ndarray', NDArrayMessage(np.ndarray)),
+    # list of floats or ints are handled by ndarrays
+    # (lists of ndarrays are handled generically as enumerated parts)
+    List[float]:
+        DataMessageField('ndarray', NDArrayMessage(List[float])),
+    List[int]:
+        DataMessageField('ndarray', NDArrayMessage(List[int])),
 
     # specialized proto message types
     #Text:
-    #    DataField('text', TextSerializer()),
+    #    DataMessageField('text', TextMessage()),
     Image:
-        DataField('image', ImageSerializer()),
+        DataMessageField('image', ImageMessage()),
     #Video:
-    #    DataField('video', VideoSerializer()),
+    #    DataMessageField('video', VideoMessage()),
     Concept:
-        DataField('concepts', ConceptSerializer()),
+        DataMessageField('concepts', ConceptMessage()),
     #Region:
-    #    DataField('regions', RegionSerializer()),
+    #    DataMessageField('regions', RegionMessage()),
     #Frame:
-    #    DataField('frames', FrameSerializer()),
+    #    DataMessageField('frames', FrameMessage()),
 
     # common python types
     PILImage:
-        DataField('image', ImageSerializer(PILImage)),
+        DataMessageField('image', ImageMessage(PILImage)),
 
     # lists of basic atomic types
     #    List[str]: json? TODO
     #    List[bytes]: json? TODO
-    List[int]:
-        DataField('ndarray', NDArraySerializer(List[int])),
-    Tuple[int]:
-        DataField('ndarray', NDArraySerializer(Tuple[int])),
-    List[float]:
-        DataField('ndarray', NDArraySerializer(List[float])),
-    Tuple[float]:
-        DataField('ndarray', NDArraySerializer(Tuple[float])),
+    List[str]:
+        DataMessageField('text', TextMessage(str), is_list=True),
 }
 
-# add serializers lists of things that are in repeated fields
+# add serializers for lists of things that are in repeated fields
 for tp, serializer in list(_SERIALIZERS.items()):
   if serializer.field_is_repeated and not serializer.is_list:
-    _SERIALIZERS[List[tp]] = DataField(serializer.field_name, serializer, is_list=True)
-    _SERIALIZERS[Tuple[tp]] = DataField(serializer.field_name, serializer, is_tuple=True)
+    _SERIALIZERS[List[tp]] = DataMessageField(serializer.field_name, serializer, is_list=True)
