bb4b7fc875d5db372bf6d381e2c939413b9dfc3a
Matthew Zeiler <zeiler@clarifai.com>
2025-02-10 22:37:01 -0500

    wrap get terminal size in try/catch

--- clarifai/utils/logging.py
+++ clarifai/utils/logging.py
@@ -142,7 +142,10 @@ def _configure_logger(name: str, logger_level: Union[int, str] = logging.NOTSET)
     logger.addHandler(handler)
   else:
     # Add the new rich handler and formatter
-    width, _ = os.get_terminal_size()
+    try:
+      width, _ = os.get_terminal_size()
+    except OSError:
+      width = 255
     handler = RichHandler(
         rich_tracebacks=True, log_time_format="%Y-%m-%d %H:%M:%S.%f", console=Console(width=width))
     formatter = logging.Formatter('%(message)s')
