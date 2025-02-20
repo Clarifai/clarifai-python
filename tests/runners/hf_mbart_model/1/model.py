920123c1a73daeabebe8e10e7952f3337829e1e3
luv-bansal <bansal22luvi@gmail.com>
2025-02-20 08:08:16 -0500

    fixing tests

--- tests/runners/hf_mbart_model/1/model.py
+++ tests/runners/hf_mbart_model/1/model.py
@@ -2,25 +2,10 @@ import os
 from typing import Iterator
 
 import torch
-from clarifai_grpc.grpc.api import resources_pb2, service_pb2
-from clarifai_grpc.grpc.api.status import status_code_pb2, status_pb2
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 from clarifai.runners.models.model_class import ModelClass
-from clarifai.utils.logging import logger
-
-NUM_GPUS = 1
-
-
-def set_output(texts: list):
-  assert isinstance(texts, list)
-  output_protos = []
-  for text in texts:
-    output_protos.append(
-        resources_pb2.Output(
-            data=resources_pb2.Data(text=resources_pb2.Text(raw=text)),
-            status=status_pb2.Status(code=status_code_pb2.SUCCESS)))
-  return output_protos
+from clarifai.runners.utils.data_handler import Output
 
 
 class MyModel(ModelClass):
@@ -30,39 +15,26 @@ class MyModel(ModelClass):
   def load_model(self):
     """Load the model here"""
     self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    logger.info(f"Running on device: {self.device}")
     checkpoints = os.path.join(os.path.dirname(__file__), "checkpoints")
-
-    for root, dirs, files in os.walk(checkpoints):
-      for f in files:
-        logger.info(os.path.join(root, f))
 
     # if checkpoints section is in config.yaml file then checkpoints will be downloaded at this path during model upload time.
     self.tokenizer = AutoTokenizer.from_pretrained(checkpoints)
     self.model = AutoModelForSeq2SeqLM.from_pretrained(
         checkpoints, torch_dtype="auto", device_map=self.device)
 
-  def predict(self, request: service_pb2.PostModelOutputsRequest
-             ) -> Iterator[service_pb2.MultiOutputResponse]:
+  def predict(self, prompt: str = "") -> Output:
     """This is the method that will be called when the runner is run. It takes in an input and
     returns an output.
     """
-    texts = [inp.data.text.raw for inp in request.inputs]
+    inputs = self.tokenizer.encode(prompt, return_tensors="pt").to(self.device)
+    outputs = self.model.generate(inputs)
+    output_text = self.tokenizer.decode(outputs[0])
+    return Output(text=output_text)
 
-    raw_texts = []
-    for t in texts:
-      inputs = self.tokenizer.encode(t, return_tensors="pt").to(self.device)
-      outputs = self.model.generate(inputs)
-      raw_texts.append(self.tokenizer.decode(outputs[0]))
-    output_protos = set_output(raw_texts)
-
-    return service_pb2.MultiOutputResponse(outputs=output_protos)
-
-  def generate(self, request: service_pb2.PostModelOutputsRequest
-              ) -> Iterator[service_pb2.MultiOutputResponse]:
+  def generate(self, prompt: str = "") -> Iterator[Output]:
     """Example yielding a whole batch of streamed stuff back."""
     raise NotImplementedError("This method is not implemented yet.")
 
-  def stream(self, request_iterator: Iterator[service_pb2.PostModelOutputsRequest]
-            ) -> Iterator[service_pb2.MultiOutputResponse]:
+  def stream(self, input_iterator) -> Iterator[Output]:
+    """Example yielding a whole batch of streamed stuff back."""
     pass
