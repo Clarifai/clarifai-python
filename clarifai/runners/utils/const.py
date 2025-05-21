dd5301469a6d6bdf0cfab2a8a3fbe39b2bc27783
Luv Bansal <luv.bansal@clarifai.com>
2025-05-21 12:52:31 +0530

    updated git hash for amd

--- clarifai/runners/utils/const.py
+++ clarifai/runners/utils/const.py
@@ -4,7 +4,7 @@ registry = os.environ.get('CLARIFAI_BASE_IMAGE_REGISTRY', 'public.ecr.aws/clarif
 
 GIT_SHA = "b8ae56bf3b7c95e686ca002b07ca83d259c716eb"
 
-AMD_GIT_SHA = "b8ae56bf3b7c95e686ca002b07ca83d259c716eb"
+AMD_GIT_SHA = "81e942130173f54927e7c9a65aabc7e32780616d"
 
 PYTHON_BASE_IMAGE = registry + '/python-base:{python_version}-' + GIT_SHA
 TORCH_BASE_IMAGE = registry + '/torch:{torch_version}-py{python_version}-{gpu_version}-' + GIT_SHA
