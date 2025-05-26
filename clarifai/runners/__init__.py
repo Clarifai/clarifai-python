984a472553c11187188408e9e675c46f8a634ec4
Sai Nivedh <sainivedh123@gmail.com>
2025-05-26 19:18:41 +0530

    cleanup fastmcp

--- clarifai/runners/__init__.py
+++ clarifai/runners/__init__.py
@@ -1,4 +1,3 @@
-from .models.mcp_class import MCPModelClass
 from .models.model_builder import ModelBuilder
 from .models.model_class import ModelClass
 from .models.model_runner import ModelRunner
@@ -8,6 +7,5 @@ __all__ = [
     "ModelRunner",
     "ModelBuilder",
     "ModelClass",
-    "MCPModelClass",
     "OpenAIModelClass",
 ]
