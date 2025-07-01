8b3e8a80b5fdabcec74dc9ec411771ce09b4f55d
Matthew Zeiler <zeiler@clarifai.com>
2025-07-01 17:05:43 -0400

    perhaps working now as a script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
+set -e
+# Before running make sure you run:
+# uv pip install -r requirements.txt -r tests/requirements.txt
+
 # You can now run this script to run the tests for the repo.
 # if you have your `clarifai config use {context}` set then you can run
 # eval $(clarifai config env) to source the variables into your shell and run_test.sh afterwards.
 # This allows you to hit different deployments of the Clarifai platform and using your own account.
 
-export PYTHONPATH=.
-
 # if CLARIFAI_USER_ID does not exist, create it
 if [ -z "$CLARIFAI_USER_ID" ]; then
+    # you need to have CLARIFAI_USER_EMAIL and CLARIFAI_USER_PASSWORD set in your environment
+    if [ -z "$CLARIFAI_USER_EMAIL" ] || [ -z "$CLARIFAI_USER_PASSWORD" ]; then
+        echo "Please set CLARIFAI_USER_EMAIL and CLARIFAI_USER_PASSWORD environment variables."
+        exit 1
+    fi
     export CLARIFAI_USER_ID="$(uv run python scripts/key_for_tests.py --get-userid)"
 fi
+# if CLARIFAI_PAT does not exist, create it
 if [ -z "$CLARIFAI_PAT" ]; then
+    # you need to have CLARIFAI_USER_EMAIL and CLARIFAI_USER_PASSWORD set in your environment
+    if [ -z "$CLARIFAI_USER_EMAIL" ] || [ -z "$CLARIFAI_USER_PASSWORD" ]; then
+        echo "Please set CLARIFAI_USER_EMAIL and CLARIFAI_USER_PASSWORD environment variables."
+        exit 1
+    fi
     export CLARIFAI_PAT="$(uv run python scripts/key_for_tests.py --create-pat)"
 fi
 # if CLARIFAI_API_BASE is not set, default to https://api.clarifai.com
@@ -22,4 +35,9 @@ if [ -z "$CLARIFAI_GRPC_BASE" ]; then
     export CLARIFAI_GRPC_BASE="api.clarifai.com"
 fi
 
-uv run pytest --cov=. --cov-report=xml:coverage/coverage.cobertura.xml --ignore=tests/runners/test_model_run_locally-container.py
+# First run the linter tests to make sure those pass.
+uv run pre-commit run --all-files
+
+# See .github/workflows/run_tests.yml as there are more combinations of tests that can be run but
+# this should cover the basics.
+uv run pytest -s --cov=. --cov-report=xml:coverage/coverage.cobertura.xml --ignore=tests/runners/test_model_run_locally-container.py
