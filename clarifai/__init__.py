08ca3b50c1f84b5fc99b4f32952b6574d5112949
Matthew Zeiler <zeiler@clarifai.com>
2025-08-15 16:18:15 -0400

    get latest protocol and grpc packages, import grpc so CLI is fixed

--- clarifai/__init__.py
+++ clarifai/__init__.py
@@ -1,1 +1,7 @@
 __version__ = "11.6.8"
+
+# Note(zeiler): this is to fix old protobuf issues which are fixed in clarifi_grpc/__init__.py
+# this is a best effort and users should really just upgrade their protobuf package.
+import clarifai_grpc
+
+_ = clarifai_grpc
