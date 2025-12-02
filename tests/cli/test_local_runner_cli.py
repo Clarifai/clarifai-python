be8d47e8296f9e06d4369471fab1eb4ec677f7be
mogith p n <mogith.nirmala@clarifai.com>
2025-12-03 01:47:59 +0530

    commented our create_runner assertion

--- tests/cli/test_local_runner_cli.py
+++ tests/cli/test_local_runner_cli.py
@@ -236,7 +236,7 @@ class TestLocalRunnerCLI:
         mock_app.create_model.assert_called_once()
         mock_model.create_version.assert_called_once()
         # TODO: Create runner is failing in CI, so commenting out for now
-        #mock_nodepool.create_runner.assert_called_once()
+        # mock_nodepool.create_runner.assert_called_once()
         mock_nodepool.create_deployment.assert_called_once()
 
     @patch("clarifai.runners.server.ModelServer")
