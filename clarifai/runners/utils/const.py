a0c3e652c6263865c70f1750e8936f8d0aadd63d
Patrick Lundquist <1460278+patricklundquist@users.noreply.github.com>
2026-02-05 15:58:32 -0500

    update sha

--- clarifai/runners/utils/const.py
+++ clarifai/runners/utils/const.py
@@ -2,7 +2,7 @@ import os
 
 registry = os.environ.get('CLARIFAI_BASE_IMAGE_REGISTRY', 'public.ecr.aws/clarifai-models')
 
-GIT_SHA = "a008c5fdd0690ce7ac44d04ec54f02722837dfc5"
+GIT_SHA = "77e4ad7daeb7a3b175702a1dc5778f22f35ff7a8"
 
 AMD_GIT_SHA = "81e942130173f54927e7c9a65aabc7e32780616d"
 
