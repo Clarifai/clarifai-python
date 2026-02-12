c37088a1be67df37099c90f3a90c4dfe22b26037
Patrick Lundquist <1460278+patricklundquist@users.noreply.github.com>
2026-02-12 15:03:38 -0800

    Add admission control (#941)
    * adds admission control that calls the model's admission control funcs if defined.

--- clarifai/runners/models/model_runner.py
+++ clarifai/runners/models/model_runner.py
@@ -110,6 +110,27 @@ class ModelRunner(BaseRunner):
         )
         return rio
 
+    @property
+    def admission_control_backoff(self) -> float:
+        """
+        The time in seconds to wait before retrying admission control. If the model defines this backoff, we use that.
+        """
+        if hasattr(self.model, 'admission_control_backoff'):
+            return self.model.admission_control_backoff
+        return super().admission_control_backoff
+
+    def check_admission(self) -> bool:
+        """
+        Check if the runner is ready to accept new work. If the model has a check_admission func, we call that.
+
+        Returns:
+          bool: True if the runner is ready to accept work, False otherwise.
+        """
+        if hasattr(self.model, 'check_admission'):
+            return self.model.check_admission()
+        else:
+            return super().check_admission()
+
     def runner_item_predict(
         self, runner_item: service_pb2.RunnerItem
     ) -> service_pb2.RunnerItemOutput:
